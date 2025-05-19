a98cba66d29e430389e9575fd0eea039c925acf0
ValsGIT <109749079+ValsGIT@users.noreply.github.com>
2025-05-19 15:39:15 -0400

    Update streamlit_app.py
    New section to display info from  SmoothieFroot

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
+
+#New section to display info from  SmoothieFroot
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
