4a4873f6bfa18ea171743cc50fcf59f1d4b0b1e2
ValsGIT <109749079+ValsGIT@users.noreply.github.com>
2025-05-19 15:52:03 -0400

    Update streamlit_app.py
    Removed the new code

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,4 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-#New section to display info from  SmoothieFroot
-#import requests
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-
+  
